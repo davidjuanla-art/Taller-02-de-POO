95b3d75e8b3010fb0867e2af0da707d482f07090
Acer 23 <Acer 23@WINDOUS_32>
2026-08-28 21:31:33 -0500

    The Square class was created, wich inherits from the abstract class GeometricFigure, and the Rhombus class, wich inherits from the Square class.

---
+++ GeometricFigures.Backend/Rhombus.cs
@@ -0,0 +1,58 @@
+﻿
+
+namespace GeometricFigures.Backend;
+
+public class Rhombus : Square
+
+{
+    //fields
+
+    private double _d1;
+    private double _d2;
+
+    // constructors 
+
+    public Rhombus(string name, double a, double d1, double d2) : base(name, a)
+    {
+        D1 = d1;
+        D2 = d2;
+    }
+
+    // properties
+
+    public double D1 { get => _d1; set => _d1 = value; }
+    public double D2 { get => _d2; set => _d2 = value; }
+
+    //public methods
+
+    public override double GetArea()
+    {
+        return (_d1 * _d2) / 2;
+    }
+
+    public override double GetPerimeter()
+    {
+        return 4 * A;
+    }
+
+    //private methods
+
+    private double ValidateD1(double D1)
+    {
+
+        if (D1 <= 0)
+        {
+            throw new Exception($"Side  {D1} must be greater than zero ");
+        }
+        return D1;
+    }
+    private double ValidateD2(double D2)
+    {
+        if (D2 <= 0)
+        {
+            throw new Exception($"Side  {D2} must be greater than zero ");
+        }
+        return D2;
+    }
+
+}
---
+++ GeometricFigures.Backend/Square.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace GeometricFigures.Backend;
+public class Square : GeometricFigure
+{
+    //fields
+
+    private double _a;
+
+    //constructors
+
+    public Square(string name, double a) 
+    {
+        A = a;
+    }
+
+    //properties
+
+    public double A
+    {
+        get => _a;
+        set => _a = ValidateA(value);
+    }
+
+    // public methods
+
+    public override double GetArea()
+    {
+        return _a * _a;
+    }
+
+    public override double GetPerimeter()
+    {
+        return 4 * _a;
+    }
+
+    // private methods
+    private double ValidateA(double A)
+    {
+        if (A <= 0)
+        {
+            throw new Exception($"Side  {A} must be greater than zero ");
+        }
+        return A;
+    }
+}
